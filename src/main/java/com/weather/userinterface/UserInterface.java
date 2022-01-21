675e8abe0ed6b4b6c8b0aa102f9077235c0a9f2b
Michal Paukszto <michal.paukszto@gmail.com>
2022-01-21 14:53:08 +0100

    small remark

--- src/main/java/com/weather/userinterface/UserInterface.java
+++ src/main/java/com/weather/userinterface/UserInterface.java
@@ -1,54 +1,47 @@
 package com.weather.userinterface;
 
-import com.weather.entry.EntryController;
-import lombok.RequiredArgsConstructor;
-
 import java.util.Scanner;
-
 
 public class UserInterface {
 
-
     public static void main(String[] args) {
+        System.out.println("Aplikacja jest uruchomiona\n");
 
+        Scanner scanner = new Scanner(System.in);
 
-            System.out.println("Aplikacja jest uruchomiona\n");
+        while (true) {
+            System.out.println("Witaj w serwisie pogodowym. Co chcesz zrobić?");
+            System.out.println("1. Dodać lokalizację do bazy");
+            System.out.println("2. Wyświetlić wszystkie dodane lokalizacje");
+            System.out.println("3. Pobrac dane o pogodzie");
+            System.out.println("0. Zamknąć aplikacje");
 
-            Scanner scanner = new Scanner(System.in);
+            int option = scanner.nextInt();
 
-            while (true) {
-                System.out.println("Witaj w serwisie pogodowym. Co chcesz zrobić?");
-                System.out.println("1. Dodać lokalizację do bazy");
-                System.out.println("2. Wyświetlić wszystkie dodane lokalizacje");
-                System.out.println("3. Pobrac dane o pogodzie");
-                System.out.println("0. Zamknąć aplikacje");
-
-                int option = scanner.nextInt();
-
-                switch (option) {
-                    case 1:
-                        //createEntry();
-                        break;
-                    case 2:
-                        //getAllEntries();
-                        break;
-                    case 3:
-                        //getWeatherData();
-                        break;
-                    case 0:
-                        return;
-                }
-                System.out.println();
+            switch (option) {
+                case 1:
+                    //createEntry();
+                    break;
+                case 2:
+                    //getAllEntries();
+                    break;
+                case 3:
+                    //getWeatherData();
+                    break;
+                case 0:
+                    return;
             }
+            System.out.println();
         }
-
-        private void getAllEntries() {
-
-        }
-
-        private void createEntry() {
-            }
     }
+
+    private void getAllEntries() {
+
+    }
+
+    private void createEntry() {
+    }
+}
 
 
 
